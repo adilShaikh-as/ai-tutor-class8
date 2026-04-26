98526a5ac1b4b6933f641447cae87e58431362f5
Adil Shaikh <adilshaikh345as@gmail.com>
2026-04-26 23:46:49 +0530

    Remove unused embedding/FAISS code

--- src/embeddings.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# Load model once
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def create_embeddings(texts):
-    embeddings = model.encode(texts, show_progress_bar=True)
-    return embeddings
